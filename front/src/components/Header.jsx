de85749bf1eb4dd034200ca1bae635e2b87ccd9d
Aléctor Alexander <alector.alexandre@gmail.com>
2024-04-02 20:43:11 -0300

    styles: adicionando estilo no bb

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -19,16 +19,19 @@ function Header() {
   return (
     <Navbar className="header mt-0 animate__animated  animate__flipInX" expand="lg">
       <Navbar.Brand><Nav.Link onClick={() => handleNavigation('/')}>Início</Nav.Link></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end mx-5">
-        <Nav>
-          <Nav.Link onClick={() => handleNavigation('/terapias')}>Terapias</Nav.Link>
-          <Nav.Link onClick={() => handleNavigation('/blog')}>Blog</Nav.Link>
-          <Nav.Link onClick={() => handleNavigation('/contato')}>Contato</Nav.Link>
-          <Nav.Link onClick={() => setOnHide(true)}>Login</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      <Navbar.Brand><Nav.Link><NotificationsComponent /></Nav.Link></Navbar.Brand>
+      <div className='reverta'> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end mx-5">
+          <Nav>
+            <Nav.Link onClick={() => handleNavigation('/terapias')}>Terapias</Nav.Link>
+            <Nav.Link onClick={() => handleNavigation('/blog')}>Blog</Nav.Link>
+            <Nav.Link onClick={() => handleNavigation('/contato')}>Contato</Nav.Link>
+            <Nav.Link onClick={() => setOnHide(true)}>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand><Nav.Link><NotificationsComponent /></Nav.Link></Navbar.Brand>
+      </div>
+
       <LoginAndRegister show={show} onHide={onHide} />
     </Navbar>
   );
